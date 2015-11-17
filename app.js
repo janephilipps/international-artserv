3be8ca96601a8a5b07707cb8e739929386f2556b
Jane Philipps <jane.philipps@gmail.com>
2015-11-16 21:01:52 -0800

    add navbar directive and template

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-var app = angular.module("artservApp", []);
+var app = angular.module('artservApp', []);
 
-app.controller("MainCtrl", ["$scope", function ($scope) {
+app.controller('MainCtrl', ['$scope', function ($scope) {
 
-  $scope.test = "hello";
+  $scope.test = 'hello';
 
 }]);
+
+app.controller('NavbarCtrl', ['$scope', function ($scope) {
+
+  $scope.nav = 'NAV';
+
+  $scope.addEvent = function () {
+    console.log('hi');
+  }
+
+}]);
+
+app.directive('appNavbar', function() {
+  return {
+    templateUrl: './views/layouts/navbar.html',
+    controller: 'NavbarCtrl'
+  };
+});
