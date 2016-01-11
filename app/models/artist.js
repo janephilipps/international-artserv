6963bfa3bdc4e598c6355ac9f88a1a841b7696ff
Jane Philipps <jane.philipps@gmail.com>
2016-01-10 19:45:58 -0800

    update model

--- app/models/artist.js
+++ app/models/artist.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Artist', {
-    firstName : {type : String, default: ''},
-    lastName : {type : String, default: ''}
+    firstName  : {type : String, default: ''},
+    lastName   : {type : String, default: ''},
+    website    : {type : String, default: ''},
+    email      : {type : String, default: ''},
+    city       : {type : String, default: ''},
+    country    : {type : String, default: ''},
+    occupation : {type : String, default: ''}
 });
