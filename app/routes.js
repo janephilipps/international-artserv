87f025ecccac219b8bc57aa9e4225067bb160b8d
Jane Philipps <jane.philipps@gmail.com>
2016-01-10 20:20:44 -0800

    add api routes

--- app/routes.js
+++ app/routes.js
@@ -2,16 +2,38 @@ var Artist = require('./models/artist');
 
 module.exports = function(app) {
 
+  var handleError = function(res, err, onSuccess) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      onSuccess();
+    }
+  }
+
   app.get('/api/artists', function(req, res) {
-    console.log('hello');
     Artist.find(function(err, artists) {
-      if (err) {
-        console.log(err);
-        res.send(err);
-      }
-      res.json(artists);
+      handleError(res, err, function() {
+        res.json(artists)
+      });
     });
   });
+
+  app.get('/api/artists/:id', function(req, res) {
+    Artist.findById(req.params.id, function(err, artist) {
+      handleError(res, err, function() {
+        res.json(artist)
+      });
+    });
+  });
+
+  // app.post('/api/artists'), function(req, res) {
+  //   Artist.find(function(err, artists) {
+  //     if (err) {
+  //       res.send(err);
+  //     }
+  //     res.json(artists);
+  //   });
+  // });
 
   app.get('*', function(req, res) {
     res.sendfile('./public/views/index.html');
